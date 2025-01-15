e3c5828f2f88a64a081d6d9eab9caaf75be9f12d
PhanTruong12 <truongpvcde170794@fpt.edu.vn>
2025-01-15 22:56:07 +0700

    Login

--- src/app/routes/Route.jsx
+++ src/app/routes/Route.jsx
@@ -2,19 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../../common/layouts/MainLayout";
 import { AbsoluteScreenPath } from "../../constants/screen";
 import HomePage from "../../pages/HomePage/HomePage";
+import LoginPage from "../../pages/Login";  // Make sure the path is correct
 
 const router = createBrowserRouter(
   [
     {
       path: AbsoluteScreenPath.Entry,
       element: <MainLayout />,
-      children: [{ index: true, element: <HomePage /> }],
+      children: [
+        { index: true, element: <HomePage /> }, // HomePage as the default index page
+        {
+          path: "/login",  // Define a route for LoginPage
+          element: <LoginPage />, 
+        },
+      ],
     },
   ],
   {
-    /**
-     * See https://reactrouter.com/en/6.27.0/upgrading/future
-     */
     future: {
       v7_normalizeFormMethod: true,
       v7_partialHydration: true,
